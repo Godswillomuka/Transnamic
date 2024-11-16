2bb2cb52dcd6da71c4b0499cb1413827d3a05e84
brian nyakundi <bazantyloffan@gmail.com>
2024-11-16 12:40:13 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-        domains: [
-          'images.pexels.com', // For Pexels images
-          'encrypted-tbn0.gstatic.com', // For Google images
-          'cdn.dealerspike.com', // For Harley-Davidson images
-          'cars.usnews.com', // For Yamaha images
-          'www.bikesmedia.in' // For Kawasaki images
-        ],
-        
-      },
+  images: {
+    domains: [
+      'images.pexels.com', // For Pexels images
+      'encrypted-tbn0.gstatic.com', // For Google images
+      'cdn.dealerspike.com', // For Harley-Davidson images
+      'cars.usnews.com', // For Yamaha images
+      'www.bikesmedia.in', // For Kawasaki images
+      'carsoko.co.ke', // For Carsoko images
+    ],
+  },
 };
 
 export default nextConfig;
