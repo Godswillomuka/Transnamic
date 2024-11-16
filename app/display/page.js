c76fab8ac3634ff7a490c278e420361cb29ccdf4
brian nyakundi <bazantyloffan@gmail.com>
2024-11-16 12:51:36 +0300

    Update page.js

--- app/display/page.js
+++ app/display/page.js
@@ -1,6 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import Navbar from '@/components/Navbar';
 
 const CarsPage = () => {
   const [cars, setCars] = useState([]); // Initialize as an empty array
@@ -20,6 +21,7 @@ const CarsPage = () => {
 
   return (
     <div className="container mx-auto p-4">
+    <Navbar />
       <h1 className="text-2xl font-bold mb-6">Available Cars</h1>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
         {cars.map((car) => (
